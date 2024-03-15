aadbcac855f352604cc14227f32b997cf634b27c
shofwahknk <shofwahknk@gmail.com>
2024-03-15 11:15:53 +0700

    view practicum

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+    public function hello() {
+        return('Hello World');
+    }
+
+    public function greeting() {
+        return view('hello') ->with('name','Shofwah Kanaka') 
+        -> with('occupation','Engineer');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -86,3 +86,9 @@ Route::resource('photos', PhotoController::class)->only([
     'index','show']);
 Route::resource('photos',PhotoController::class)->except([
     'create','store','update','destroy']);
+
+// Route::get('/greeting',function(){
+//     return view('hello',['name'=>'Shofwah']);
+// }); 
+
+Route::get('/greeting',[WelcomeController::class,'greeting']); 
